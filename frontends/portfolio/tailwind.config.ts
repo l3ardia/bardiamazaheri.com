d4b601f119fd18f0c17271a59caf5b0475081662
Bardia Mazaheri <bardia_hm@yahoo.com>
2025-07-19 18:48:15 +1000

    style(about): enhance timeline visuals

--- frontends/portfolio/tailwind.config.ts
+++ frontends/portfolio/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { amber } from "tailwindcss/colors";
 
 export default {
   content: [
@@ -11,6 +12,7 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        secondary: amber,
       },
     },
   },
